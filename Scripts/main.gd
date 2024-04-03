b1fd3be0b5139103c5be05eeb187bed4010bd64b
Kiiiw3y <m.nickita@web.de>
2024-04-03 23:04:23 +0200

    Finished all tasks
    You can basicly play the game now, however you cant loose for now.
    press ESC to quit

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -25,6 +25,24 @@ extends Node2D
 #Keys
 @onready var key_q = $"Control/VBoxContainer/1/Laundry/Key_Left"
 @onready var key_e = $"Control/VBoxContainer/1/Laundry/Key_Right"
+@onready var key_u = $"Control/VBoxContainer/1/Cooking/Key_Left"
+@onready var key_p = $"Control/VBoxContainer/1/Cooking/Key_Right"
+@onready var key_7 = $"Control/VBoxContainer/1/Socialising/Key_Left"
+@onready var key_9 = $"Control/VBoxContainer/1/Socialising/Key_Right"
+@onready var key_a = $"Control/VBoxContainer/2/Shopping/Key_Left"
+@onready var key_d = $"Control/VBoxContainer/2/Shopping/Key_Right"
+@onready var key_enter = $"Control/VBoxContainer/2/Fitness/Key_Left"
+@onready var key_backspace = $"Control/VBoxContainer/2/Fitness/Key_Right"
+@onready var key_ctrl = $"Control/VBoxContainer/3/Travell/Key_Left"
+@onready var key_space = $"Control/VBoxContainer/3/Travell/Key_Right"
+@onready var key_h = $"Control/VBoxContainer/3/Work/Key_Left"
+@onready var key_k = $"Control/VBoxContainer/3/Work/Key_Right"
+@onready var key_1 = $"Control/VBoxContainer/3/Childcare/Key_Left"
+@onready var key_3 = $"Control/VBoxContainer/3/Childcare/Key_Right"
+
+
+
+
 
 
 @onready var tasksPB = [laundryhp,cookinghp,socialisinghp,shoppinghp,fitnesshp,travellhp,workhp,childcarehp]
@@ -44,12 +62,62 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#Top Left Task:
 	if Input.is_action_just_pressed("Q"):
 		laundryhp.health += 0.2
 		key_q.get_child(0).play("Keypress")
 	if Input.is_action_just_pressed("E"):
 		laundryhp.health += 0.2
 		key_e.get_child(0).play("Keypress")
+	#Top Middle Task:
+	if Input.is_action_just_pressed("U"):
+		cookinghp.health += 0.2
+		key_u.get_child(0).play("Keypress")
+	if Input.is_action_just_pressed("P"):
+		cookinghp.health += 0.2
+		key_p.get_child(0).play("Keypress")
+	#Top Right Task:
+	if Input.is_action_just_pressed("7"):
+		socialisinghp.health += 0.2
+		key_7.get_child(0).play("Keypress")
+	if Input.is_action_just_pressed("9"):
+		socialisinghp.health += 0.2
+		key_9.get_child(0).play("Keypress")
+	#Middle Left Task:
+	if Input.is_action_just_pressed("A"):
+		shoppinghp.health += 0.2
+		key_a.get_child(0).play("Keypress")
+	if Input.is_action_just_pressed("D"):
+		shoppinghp.health += 0.2
+		key_d.get_child(0).play("Keypress")
+	#Middle Right Task:
+	if Input.is_action_just_pressed("ENTER"):
+		fitnesshp.health += 0.2
+		key_enter.get_child(0).play("Keypress")
+	if Input.is_action_just_pressed("BACKSPACE"):
+		fitnesshp.health += 0.2
+		key_backspace.get_child(0).play("Keypress")
+	#Bottom Left Task:
+	if Input.is_action_just_pressed("L_CTRL"):
+		travellhp.health += 0.2
+		key_ctrl.get_child(0).play("Keypress")
+	if Input.is_action_just_pressed("SPACEBAR"):
+		travellhp.health += 0.2
+		key_space.get_child(0).play("Keypress")
+	#Bottom Middle Task:
+	if Input.is_action_just_pressed("H"):
+		workhp.health += 0.2
+		key_h.get_child(0).play("Keypress")
+	if Input.is_action_just_pressed("K"):
+		workhp.health += 0.2
+		key_k.get_child(0).play("Keypress")	
+	#Bottom Right Task:
+	if Input.is_action_just_pressed("1"):
+		childcarehp.health += 0.2
+		key_1.get_child(0).play("Keypress")
+	if Input.is_action_just_pressed("3"):
+		childcarehp.health += 0.2
+		key_3.get_child(0).play("Keypress")
 
 func increaseDifficulty():
 	if currentDifficulty < tasksPB.size():
@@ -57,6 +125,9 @@ func increaseDifficulty():
 		tasks[currentDifficulty].activateTask()
 		currentDifficulty += 1
 
+func _input(event):
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
 
 func _on_increase_difficulty_timeout():
 	if currentDifficulty < tasksPB.size():
