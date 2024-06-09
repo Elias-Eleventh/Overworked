ca796185f4dfba0ce70cac705370543cbc334ac7
Kiiiw3y <m.nickita@web.de>
2024-06-09 19:48:37 +0200

    Added Sounds
    also added some minor documentation

--- Task.gd
+++ Task.gd
@@ -1,17 +1,34 @@
-extends TextureRect
+class_name Task
 
-@onready var key_left = $Key_Left
-@onready var key_right = $Key_Right
-@onready var progress_bar = $ProgressBar
-@onready var label = progress_bar.get_child(0)
+extends TextureRect
+## Reference to the left Key image.
+@onready var key_left : TextureRect = $Key_Left
+## Reference to the right Key image.
+@onready var key_right : TextureRect = $Key_Right
+## Reference to the progress bar.
+@onready var progress_bar : ProgressBar = $ProgressBar
+## Reference to the label/text of the progress bar.
+@onready var label : Label = progress_bar.get_child(0)
+## Reference to the Audioplayer playing at the start.
+@onready var audio_start : AudioStreamPlayer2D = $AudioStart
+## Reference to the Audioplayer playing in a loop.
+@onready var audio_loop : AudioStreamPlayer2D = $AudioLoop
+## Reference to the Timer for the Audioplayer.
+@onready var audio_timer : Timer = $AudioTimer
 
 
 signal gameOver
+## determines if a Loop is played constantly or with a break inbetween.
+@export var constantLoop = false
+var newText : String = ""
+## determines how much hp is lost per second.
+var hps : float = 1
+## Current Health.
+var health : float = (randi()% 11 + (10/Globals.difficulty))
+## shows if Task is currently running.
+var isActive : bool
 
-var newText = ""
-var hps = 1
-var health = (randi()% 11 + (10/Globals.difficulty))
-var isActive 
+
 func _ready():
 	isActive = false
 	self.visible = false
@@ -24,6 +41,7 @@ func _ready():
 	key_left.visible = false
 	key_right.visible = false
 	
+## Activates the Task and sets it to active and turns on visibility on all elements.
 func activateTask():
 	isActive = true
 	print("Task started: " + self.name + " with " + String.num(health) + " seconds to live")
@@ -31,7 +49,13 @@ func activateTask():
 	progress_bar.visible = true
 	key_left.visible = true
 	key_right.visible = true
+	playAudio()
 
+func playAudio() -> void:
+	audio_start.play(0.0)
+
+func playAudioLoop() -> void:
+	audio_loop.play(0.0)
 
 func _process(delta):
 	if isActive:
@@ -39,8 +63,22 @@ func _process(delta):
 		newText = String.num(progress_bar.health,2)
 		label.text = newText
 
+## switches to the gameover Scene when health reaches 0.
 func _on_progress_bar_dead():
 	isActive = false
 	print("Failed task: " + self.name)
 	if(!Globals.debugMode):
 		get_tree().change_scene_to_file("res://Scenes/game_over!.tscn")
+
+## starts the Audioloop.
+func _on_audio_start_finished():
+	playAudioLoop()
+## plays the audio in a loop depending if constantLoop is set to true, else start timer.
+func _on_audio_loop_finished():
+	if constantLoop:
+		playAudioLoop()
+	else:
+		audio_timer.start()
+## plays the audioLoop if constantLoop is set to false.
+func _on_audio_timer_timeout():
+	playAudioLoop()
