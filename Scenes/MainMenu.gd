47c14df595730ae6c524e2523cf914d2edabab8e
Elias <elias.vincentz@gmx.de>
2024-08-01 17:16:47 +0200

    Adding Options Menu

--- Scenes/MainMenu.gd
+++ Scenes/MainMenu.gd
@@ -1,19 +1,21 @@
+class_name MainMenu
 extends Control
 
 @onready var debugButton = $CheckButton
+@onready var start_button = $start_button
+@onready var options_button = $options_button
+@onready var quit_button = $quit_button
+@onready var start_level = preload("res://Scenes/tutorial.tscn") as PackedScene
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
+
+
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/options_menu.tscn")
 
 
 func _on_quit_pressed():
